f30cef8b6b52cce038fd533ab165ac4ae3888262
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2026-08-12 10:57:03 +0200

    Update fpgs.R

--- R/fpgs.R
+++ R/fpgs.R
@@ -1,3 +1,26 @@
+#' Estimate the Full Prognostic Score
+#'
+#' Estimates the two-dimensional Full Prognostic Score (FPGS) using
+#' parametric or random forest(rf) outcome models for continuous or binary
+#' outcomes.
+#'
+#' @param data A data frame containing the observed data.
+#' @param outcome Character string giving the name of the outcome variable.
+#' @param treatment Character string giving the name of the treatment variable.
+#' @param covariates Character vector giving the names of the covariates.
+#' @param outcome_type Type of outcome: \code{"continuous"} or \code{"binary"}.
+#' @param method Estimation method: \code{"parametric"} or \code{"rf"}.
+#' @param folds Number of folds used for cross-fitting when
+#'   \code{method = "rf"}. Default is 5.
+#' @param ... Additional arguments passed to the random forest estimation
+#'   functions.
+#'
+#' @return An object of class \code{"fpgs"} containing the estimated
+#'   FPGS and information required by the downstream
+#'   treatment effect estimators.
+#'
+#' @export
+
 fpgs <- function(data,
                  outcome,
                  treatment,
