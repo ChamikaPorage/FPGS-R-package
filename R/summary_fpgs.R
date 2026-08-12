969bf866291e5111aa2d8f05f74c84c79795810a
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2026-08-12 13:24:28 +0200

    Update summary_fpgs.R

--- R/summary_fpgs.R
+++ R/summary_fpgs.R
@@ -1,9 +1,20 @@
+#' Summarize an FPGS Object
+#'
+#' Computes and displays treatment effect estimates obtained using the
+#' available FPGS-based estimators.
+#'
+#' @param object An object of class \code{"fpgs"} returned by \code{fpgs()}.
+#' @param ... Additional arguments passed to \code{fpgs_all()}.
+#'
+#' @return A data frame containing the FPGS-based treatment effect estimates,
+#'   returned invisibly.
+#'
 #' @export
 summary.fpgs <- function(object, ...) {
   
-  res <- fpgs_all(object)
+  res <- fpgs_all(object, ...)
   
-  cat("\nFPGS Estimates\n")
+  cat("\nFPGS Treatment Effect Estimates\n")
   cat("----------------------------------\n")
   
   print(res)
