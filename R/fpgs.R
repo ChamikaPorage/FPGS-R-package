99ed553be6f9e97cb35508dba4ccc1a117ce46c5
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2026-08-31 00:32:53 +0200

    Update fpgs.R

--- R/fpgs.R
+++ R/fpgs.R
@@ -1,26 +1,27 @@
 #' Estimate the Full Prognostic Score
 #'
 #' Estimates the two-dimensional Full Prognostic Score (FPGS) using
-#' parametric or random forest (rf) outcome models for continuous or binary
+#' parametric or random forest outcome models for continuous or binary
 #' outcomes.
 #'
 #' @param data A data frame containing the observed data.
 #' @param outcome Character string giving the name of the outcome variable.
 #' @param treatment Character string giving the name of the treatment variable.
-#' @param covariates Character vector giving the names of the covariates.
+#' @param covariates Character vector giving the names of the pre treatment variables.
 #' @param outcome_type Type of outcome: \code{"continuous"} or \code{"binary"}.
 #' @param method Estimation method: \code{"parametric"} or \code{"rf"}.
+#'   where \code{"rf"} denotes random forest.
 #' @param folds Number of folds used for cross-fitting when
 #'   \code{method = "rf"}. Default is 5.
-#' @param ... Additional arguments passed to the random forest estimation
-#'   functions.
+#' @param ... Additional arguments passed to the random forest fitting
+#'   functions when \code{method = "rf"}, such as \code{num.trees},
+#'   \code{mtry}, and \code{min.node.size}.
 #'
 #' @return An object of class \code{"fpgs"} containing the estimated
-#'   FPGS and information required by the downstream
-#'   treatment effect estimators.
+#'   FPGS and information required by the downstream treatment effect
+#'   estimators.
 #'
 #' @export
-
 fpgs <- function(data,
                  outcome,
                  treatment,
@@ -29,55 +30,57 @@ fpgs <- function(data,
                  method = c("parametric", "rf"),
                  folds = 5,
                  ...) {
-  
+
   outcome_type <- match.arg(outcome_type)
   method <- match.arg(method)
-  
+
   if (outcome_type == "continuous" && method == "parametric") {
-    return(
-      fpgs_continuous_parametric(
-        data,
-        outcome,
-        treatment,
-        covariates
-      )
+
+    fit <- fpgs_continuous_parametric(
+      data,
+      outcome,
+      treatment,
+      covariates
     )
   }
-  
+
   if (outcome_type == "continuous" && method == "rf") {
-    return(
-      fpgs_rf(
-        data,
-        outcome,
-        treatment,
-        covariates,
-        folds = folds,
-        ...
-      )
+
+    fit <- fpgs_continuous_rf(
+      data,
+      outcome,
+      treatment,
+      covariates,
+      folds = folds,
+      ...
     )
   }
-  
+
   if (outcome_type == "binary" && method == "parametric") {
-    return(
-      fpgs_binary(
-        data,
-        outcome,
-        treatment,
-        covariates
-      )
+
+    fit <- fpgs_binary_parametric(
+      data,
+      outcome,
+      treatment,
+      covariates
     )
   }
-  
+
   if (outcome_type == "binary" && method == "rf") {
-    return(
-      fpgs_binary_rf(
-        data,
-        outcome,
-        treatment,
-        covariates,
-        folds = folds,
-        ...
-      )
+
+    fit <- fpgs_binary_rf(
+      data,
+      outcome,
+      treatment,
+      covariates,
+      folds = folds,
+      ...
     )
   }
+
+  # Store information needed by downstream estimators
+  fit$outcome_type <- outcome_type
+  fit$method <- method
+
+  fit
 }
