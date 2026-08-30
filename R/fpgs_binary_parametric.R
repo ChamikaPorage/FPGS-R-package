cfeb13d4f3b05f56d5d2e5e6a3178eb93b3efffc
ChamikaPorage <92798615+ChamikaPorage@users.noreply.github.com>
2026-08-31 00:34:24 +0200

    Update and rename fpgs_binary.R to fpgs_binary_parametric.R

--- R/fpgs_binary_parametric.R
+++ R/fpgs_binary_parametric.R
@@ -1,47 +1,40 @@
+
 #' Estimate Binary FPGS Using Parametric Regression
 #'
 #' @param data Data frame containing the observed data.
 #' @param outcome Binary outcome variable name.
 #' @param treatment Treatment variable name.
-#' @param covariates Covariate names. If NULL, all variables other than
+#' @param covariates Pre treatment variables names. If NULL, all variables other than
 #'   the outcome and treatment are used.
 #'
 #' @return An object of class \code{"fpgs"}.
 #'
 #' @keywords internal
 
-fpgs_binary <- function(data,
+fpgs_binary_parametric <- function(data,
                         outcome,
                         treatment,
                         covariates = NULL) {
-  
+
   dat <- as.data.frame(data)
-  
+
   if (is.null(covariates)) {
     covariates <- setdiff(names(dat), c(outcome, treatment))
   }
-  
+
   tr <- dat[[treatment]]
-  
+
   form <- as.formula(
     paste(outcome, "~", paste(covariates, collapse = " + "))
   )
-  
-  fit1 <- glm(
-    form,
-    data = dat[tr == 1, , drop = FALSE],
-    family = binomial()
-  )
-  
-  fit0 <- glm(
-    form,
-    data = dat[tr == 0, , drop = FALSE],
-    family = binomial()
-  )
-  
+
+  fit1 <- glm(form, data = dat[tr == 1, , drop = FALSE], family = binomial())
+
+  fit0 <- glm(form, data = dat[tr == 0, , drop = FALSE], family = binomial())
+
   mu1_hat <- predict(fit1, newdata = dat, type = "response")
   mu0_hat <- predict(fit0, newdata = dat, type = "response")
-  
+
   out <- list(
     data = dat,
     outcome = outcome,
@@ -56,8 +49,8 @@ fpgs_binary <- function(data,
     outcome_type = "binary",
     method = "parametric"
   )
-  
+
   class(out) <- "fpgs"
-  
+
   out
 }
